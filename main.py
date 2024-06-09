6621c5908c599cfb910a0b44c88886b3630c5bc7
Ruukee <kaluga.babaev@gmail.com>
2024-06-09 18:37:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+str_ = 'Hello'
+len_num = len(str_)
+print(len_num)
+
+int_first = 11
+int_second = 22
+int_summa = int_first + int_second
+int_diff = int_second - int_first
+print(int_summa, int_diff)
+
+int_first = 11
+int_second = 22
+int_third = 33
+int_ = (int_first + int_second + int_third)
+int_mean = (int_//3)
+print(int_mean)
+
+first_string = ',  Вторник'
+second_string = 'Понедельник'
+print(second_string, first_string)
+
+int_a = 2
+int_b = 4
+int_c = 6
+int_f = ((((int_a*int_b) + (int_a*int_c))**3)/2)
+print(int_f)
+
+
+
